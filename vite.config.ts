1c53c10612d87f554ea3bf483970ce43037fbabb
Jay Laiche <jay@jaylaiche.com>
2024-05-05 23:08:55 -0400

    Use spaces for identation

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import {
-	vitePlugin as remix,
-	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [remixCloudflareDevProxy(), remix()],
+  plugins: [remixCloudflareDevProxy(), remix()],
 });
