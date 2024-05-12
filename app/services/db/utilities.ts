6eb1b94cab769edf7b9eacdc0e6269f63da1b666
Jay Laiche <jay@jaylaiche.com>
2024-05-12 17:56:54 -0400

    Beginning to add auth

---
+++ app/services/db/utilities.ts
@@ -0,0 +1,8 @@
+import { AppLoadContext } from "@remix-run/cloudflare";
+import { drizzle } from "drizzle-orm/d1";
+import * as schema from "db/schema";
+
+export const getDB = (context: AppLoadContext) =>
+  drizzle(context.cloudflare.env.DB, { schema });
+
+export type AppDB = ReturnType<typeof getDB>;
--- db/schema.ts
+++ db/schema.ts
@@ -2,13 +2,14 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const books = sqliteTable("books", {
   id: integer("id").primaryKey(),
-  title: text("title"),
-  author: text("author"),
-  description: text("description"),
+  title: text("title").unique().notNull(),
+  author: text("author").notNull(),
+  description: text("description").notNull(),
 });
 
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey(),
-  username: text("username"),
-  password: text("password"),
+  username: text("username").unique().notNull(),
+  password: text("password").notNull(),
+  salt: text("salt").notNull(),
 });
